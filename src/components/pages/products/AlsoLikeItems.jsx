3d27a9a94c9ace390b2a8cca4b9728f1641ff7e0
Giorgi Bebia <elogiogio@gmail.com>
2025-03-01 15:48:28 +0400

    Fix AlsoLikeItems Component

--- src/components/pages/products/AlsoLikeItems.jsx
+++ src/components/pages/products/AlsoLikeItems.jsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import { scrollToTop } from "../../../utils/scrollToTop";
+
 export function AlsoLikeItems({ item }) {
+  const a = encodeURIComponent(item.newlink);
+
+  const navigate = useNavigate();
+
   return (
     <div className="w-[350px] mobile:w-[327px] h-[471px] mobile:h-[265px] flex flex-col justify-between">
       <img className="tablet:hidden" src={item.img} alt={item.name} />
@@ -15,7 +22,14 @@ export function AlsoLikeItems({ item }) {
       <span className="text-center font-manrope font-bold mobile:text-[24px]">
         {item.name}
       </span>
-      <button className="w-[160px] h-[48px] bg-[#D87D4A] text-[13px] font-manrope text-white mx-auto hover:bg-[#FBAF85]">
+
+      <button
+        className="w-[160px] h-[48px] bg-[#D87D4A] text-[13px] font-manrope text-white mx-auto hover:bg-[#FBAF85]"
+        onClick={() => {
+          navigate(`/product/${a}`);
+          scrollToTop();
+        }}
+      >
         SEE PRODUCT
       </button>
     </div>
