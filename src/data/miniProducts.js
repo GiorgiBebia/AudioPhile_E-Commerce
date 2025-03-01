f50374ed9257910c0e61782e4d2f93e8461aeb86
Giorgi Bebia <elogiogio@gmail.com>
2025-03-01 16:00:35 +0400

    Change miniProduct.js Data File

--- src/data/miniProducts.js
+++ src/data/miniProducts.js
@@ -1,36 +1,42 @@
 export const miniProducts = [
   {
     name: "XX99 MARK I",
+    newlink: "XX99 MARK I HEADPHONES",
     img: "img/xx99mark1Mini.png",
     tabletImg: "img/tabletAlsoLikeXX99Mark1.png",
     mobileImg: "img/mobileAlsoLikeXX99Mark1.png",
   },
   {
     name: "XX59",
+    newlink: "XX59 HEADPHONES",
     img: "img/xx59Mini.png",
     tabletImg: "img/tabletAlsoLikeXX59.png",
     mobileImg: "img/mobileAlsoLikeXX59.png",
   },
   {
     name: "ZX9 SPEAKER",
+    newlink: "ZX9 SPEAKER",
     img: "img/zx9SpeakerMini.png",
     tabletImg: "img/tabletAlsoLikeZX9Speaker.png",
     mobileImg: "img/mobileAlsoLikeZX9Speaker.png",
   },
   {
     name: "XX99 MARK II",
+    newlink: "XX99 MARK II HEADPHONES",
     img: "img/xx99Mark2Mini.png",
     tabletImg: "img/tabletAlsoLikeXX99Mark2.png",
     mobileImg: "img/mobileAlsoLikeXX99Mark2.png",
   },
   {
     name: "ZX7 SPEAKER",
+    newlink: "ZX7 SPEAKER",
     img: "img/zx9SpeakerMini.png",
     tabletImg: "img/tabletAlsoLikeZX7Speaker.png",
     mobileImg: "img/mobileAlsoLikeZX7Speaker.png",
   },
   {
     name: "YX1 WIRELESS",
+    newlink: "YX1 WIRELESS HEADPHONES",
     img: "img/yx1EarphonesMini.png",
     tabletImg: "img/tabletAlsoLikeYX1Wireless.jpg",
     mobileImg: "img/mobileAlsoLikeYX1Wireless.jpg",
